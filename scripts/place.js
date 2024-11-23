803efa26f2a1237ea6a54759d498b91cb87be9e9
carcent <carli.centurion@gmail.com>
2024-11-23 00:50:19 -0300

    changes

--- scripts/place.js
+++ scripts/place.js
@@ -1,13 +1,16 @@
 function CalculateWindChill(temperature, windSpeed, unit) {
     return unit ==='C'
-    ?(13.12+0.3215 * temperature -11.37 * Math.pow(windSpeed, 0.16) +0.4275 * temperature * Math.pow(windSpeed, 0.16));
+    ? (13.12+0.3215 * temperature -11.37 * Math.pow(windSpeed, 0.16) +0.4275 * temperature * Math.pow(windSpeed, 0.16));
 }
+
+let temperature = 4;
+let windSpeed= 7.9;
+let windChillC;
 
 if ((temperature <= 10 && windSpeed>4.8)){
-    let windChillC = CalculateWindChill(4,7.9, 'C');
-    console.log('Wind Chill:${windChillC}');
+    let windChillC = CalculateWindChill(temperature, windSpeed, 'C');
+    document.getElementById('windChillResult').innerHTML = `Wind Chill: ${windChillC.toFixed(2)}°C`;
 } else {
-    windChillC = "N/A"
+    windChillC = "N/A";  
+    document.getElementById('windChillResult').innerHTML = `Wind Chill: ${windChillC}`;
 }
-
-return windChillC;
