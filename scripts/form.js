ce33844686e8c3b1b9a6680fb79ab0e4c785e040
carcent <carli.centurion@gmail.com>
2024-12-09 15:35:23 -0300

    week5

---
+++ scripts/form.js
@@ -0,0 +1,59 @@
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+const selectElement = document.getElementById("product-select");
+
+const defaultOption = document.createElement("option");
+defaultOption.value = "";
+defaultOption.textContent = "Select a Product...";
+selectElement.appendChild(defaultOption);
+
+products.forEach(product =>{
+  const option = document.createElement("option");
+  option.value = product.id;
+  option.textContent = product.name;
+  selectElement.appendChild(option);
+})
+
+let reviewCount = localStorage.getItem('reviewCount');
+if (!reviewCount) {
+  reviewCount = 0; 
+}
+
+document.getElementById('reviewForm').addEventListener('submit', function (event) {
+  event.preventDefault(); 
+
+  reviewCount++; 
+  
+  localStorage.setItem('reviewCount', reviewCount); 
+  alert("Your review has been submitted!"); 
+});
+
+if (document.getElementById('reviewCount')) {
+document.getElementById('reviewForm').textContent=reviewCount
+}
